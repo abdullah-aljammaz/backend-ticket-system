6123c316935c04d76d2be5f2e931d027382100b7
abdullah-aljammaz <crm1999@outlook.sa>
2023-12-23 21:20:06 +0300

    hash Password

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { connectDB, prisma } from "./config/db";
 import express, { ErrorRequestHandler, Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { CronJob } from "cron";
+const argon2 = require('argon2');
 
 const app = express();
 const PORT = 3006;
 
 app.use(express.json());
 connectDB();
+
 async function deleteExpiredEvents() {
   console.log("test")
   const EventTimeEnd = await prisma.event.findMany({
@@ -57,21 +59,40 @@ async function isAdmin(userId: string): Promise<boolean> {
 app.post("/user/register", async (req: Request, res: Response) => {
   let newUser = req.body as User;
   console.log(newUser);
-  await prisma.user.create({ data: newUser });
-  res.json("User Created");
+  try {
+    const hash = await argon2.hash(newUser.password);
+    newUser.password = hash
+    await prisma.user.create({ data: newUser });
+    res.json("User Created");
+  } catch (err) {
+    res.json("Error Create Acount")
+  }
+
 });
 
 // login
 
 app.post("/user/login", async (req: Request, res: Response) => {
   const { email, password } = req.body as User;
-  const users = await prisma.user.findFirst({
-    where: { email: email, password: password },
-  });
-  if (users == null) {
-    return res.json("Username Or Password Is Not correct");
-  } else {
-    return res.json("Login successfully");
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: { email: email },
+    });
+      if (!user) {
+        return res.json("Username Or Password Is Not correct");
+      }
+      // Verify password using Argon2
+    let passwordMatch = await argon2.verify(user.password,password);
+    if (passwordMatch) {
+      // Login successful
+      return res.json("Login successfully");
+    } else {
+      return res.json("Username Or Password Is Not correct");
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json("Internal server error");
   }
 });
 
@@ -84,16 +105,7 @@ app.get("/user/get_all", async (req: Request, res: Response) => {
 // Get User With Id
 app.get("/user/get_with_id/:id", async (req: Request, res: Response) => {
   let { id } = req.params;
-<<<<<<< HEAD
   let user = await prisma.user.findMany({ where: { id: id }});
-=======
-  let user = await prisma.user.findMany({
-    where: { id: id },
-    include: {
-      event: true,
-    },
-  });
->>>>>>> 9966dd57f53f2eb7e9128f6fd3d29b0422a2e2ee
   res.json(user);
 });
 
@@ -115,22 +127,9 @@ app.put("/user/update/:id", async (req: Request, res: Response) => {
 // Create Event
 app.post("/event/create", async (req: Request, res: Response) => {
   let newEvent = req.body as Event;
-<<<<<<< HEAD
-  let admin_check = await prisma.user.findFirst({where:{id:newEvent.admin}})
+  let admin_check = await prisma.user.findFirst({where:{id:newEvent.admin_id}})
   await prisma.event.create({ data: newEvent });
   res.json("event added");
-=======
-
-  const isAdminUser = await isAdmin(newEvent.admin_id);
-  if (isAdminUser) {
-    // User is an admin, allow event creation
-    await prisma.event.create({ data: newEvent });
-    return res.json("Event added");
-  } else {
-    // User is not an admin, deny access
-    return res.json("You don't have access");
-  }
->>>>>>> 9966dd57f53f2eb7e9128f6fd3d29b0422a2e2ee
 });
 
 // Get All Events
