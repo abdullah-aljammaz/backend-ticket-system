60111e43e73676653c6707ff5c2601adfbb2f75d
abdullah-aljammaz <crm1999@outlook.sa>
2023-12-23 23:13:46 +0300

    test

--- test.ts
+++ test.ts
@@ -1,10 +1,8 @@
-let d = new Date();
-console.log(`Hoursuts: ${d.getUTCHours()}`); // Hours
-console.log(`hours: ${d.getHours()}`);
-console.log(`Min: ${d.getUTCMinutes()}`);
-console.log(`Sec: ${d.getUTCSeconds()}`);
-console.log(`Day: ${d.getDay()}`);
-console.log(`utcDay: ${d.getUTCDay()}`);
-console.log(`Month: ${d.getMonth()}`);
-console.log(`Year: ${d.getFullYear()}`);
-console.log(`al: ${d.toJSON()}`);
+function percant(number1:number,number2:number){
+  let number = (number2/100 );
+  let discoubt_number = (number*number1)
+  let final = number1 - discoubt_number
+  return final
+}
+
+console.log(percant(4000,15))
